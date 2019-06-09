656539ca1db0e4c571c7abe315ea95ada2c49395
DenisDorogov <denisdorogov@yandex.ru>
2019-06-09 23:23:40 +0500

    Домашнее задание № 2 сделано.

--- index.php
+++ index.php
@@ -1,30 +1,95 @@
 <?php
-$a =   5; 
-$b =   '05';
-$time = getdate();
-$x = 7;
-$y = 8;
+function sum($x, $y) {return $x + $y;}
+function dif($x, $y) {return $x - $y;}
+function mult($x, $y) {return $x * $y;}
+function div($x, $y) {
+  if ($y >= 0) {
+    return $x / $y;
+  } else echo 'Делить на "0" нельзя';
+}
+function mathOperation($arg1, $arg2, $operation) {
+  switch ($operation) {
+    case 'плюс' : 
+      return sum($arg1, $arg2);
+      break;
+    case 'вычесть' : 
+      return dif($arg1, $arg2);
+      break;
+    case 'умножить' : 
+      return mult($arg1, $arg2);
+      break;
+    case 'разделить' : 
+      return div($arg1, $arg2);
+      break;
+  }
+}
 
-var_dump ( $a ==  $b ); // Почему true?
-echo '<br>Нет сравнения типов, всё привелось к integer.<br>';
+$a = mt_rand(-1000; 1000);
+$b = mt_rand(-1000; 1000);
+echo "Задание 1: A = $a, B = $b "
+if ($a >= 0 and $b >= 0) {
+  echo "Разность равна " . div($a, $b) . PHP_EOL;
+} elseif ($a < 0 and $b < 0) {
+  echo "Произведение равно " . mult($a, $b) . PHP_EOL;
+} else echo "Сумма равна " . sum($a, $b) . PHP_EOL;
 
-var_dump (( int ) '012345' ); // Почему 12345? 
-echo '<br>Строку привели к целому числу.<br>';  
+$a = mt_rand(0; 15);
+echo "Задание 2: ";
+switch ($a) {
+  case 0 : echo $a++ . ", ";
+  case 1 : echo $a++ . ", ";
+  case 2 : echo $a++ . ", ";
+  case 3 : echo $a++ . ", ";
+  case 4 : echo $a++ . ", ";
+  case 5 : echo $a++ . ", ";
+  case 6 : echo $a++ . ", ";
+  case 7 : echo $a++ . ", ";
+  case 8 : echo $a++ . ", ";
+  case 9 : echo $a++ . ", ";
+  case 10 : echo $a++ . ", ";
+  case 11 : echo $a++ . ", ";
+  case 12 : echo $a++ . ", ";
+  case 13 : echo $a++ . ", ";
+  case 14 : echo $a++ . ", ";
+  case 15 : echo "15";
+}
 
-var_dump (( float ) 123.0   ===   ( int ) 123.0 );  // Почему false? 
-echo '<br>Типы данных различны<br>'; 
+//Пятое задание делал в первом уроке
+function power($val, $pow) {
+  if ($pow != 1) return $val * power($val, $pow - 1)
+};
+echo writeTime();
+function writeTime() {
+  $today = getdate();
+  $h = today['hours'];
+  $m = today['minutes'];
+  $s = today['seconds'];
+  echo "Текущее время: "$h . named('h', $h) . $m . named('m', $m) . $s . named('s', $s) . ;
+}
+function named($type, $val) {
+  switch ($type) {
+    case 'h' : 
+      if ($val % 10 > 4 || $val > 10 and $val < 21)  return ' часов ';
+      elseif ($val % 10 = 1) return ' час ';
+      else return ' часа ';
+      break;
+    case 'm' : 
+      if ($val % 10 > 4 || $val > 10 and $val < 21)  return ' минут ';
+      elseif ($val % 10 = 1) return ' минута ';
+      else return ' минуты ';
+      break;
+    case 's' : 
+      if ($val % 10 > 4 || $val > 10 and $val < 21)  return ' секунд ';
+      elseif ($val % 10 = 1) return ' секунда ';
+      else return ' секунды ';
+      break;
+  }
+}
 
-var_dump (( int ) 0   ===   ( int ) 'hello, world' );// Почему true?
-echo '<br>Строка при смене типа, преобразуется в "0"<br>'; 
-
-echo "<h1>Текущее время равно: $time[hours] часов $time[minutes] минут $time[seconds] секунд</h1>";
-
-echo "Сначала переменная 1 = $x, а переменная 2 = $y<br>";
-//$x = $x - $y;
-//$y = $y + $x;
-//$x = $y - $x;
-$y = ($x += $y -= $x) - $y;
-echo "Теперь переменная 1 = $x, а переменная 2 = $y";
 
 
+
+
+
+print 'Hello world';
 ?>
