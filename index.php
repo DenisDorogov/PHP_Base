da234dca80c5a27673cb5a456d6d79d3bff2c459
DenisDorogov <denisdorogov@yandex.ru>
2019-06-17 15:29:20 +0500

    Поработал еще. Не получается вывести транслитуралы. Почему-то не наполняется строка $resultStr в функции transLitString. Проблему пока не решил. 5 задание сделано. 6 начал работать.

--- index.php
+++ index.php
@@ -43,4 +43,88 @@ foreach ($region as $name => $value) {
 };
 
 // 4 задание
-Пока не успеваю доделать. Постараюсь сдать со следующим заданием.
+echo '<br>4 задание<br><br>';
+$translit = [
+  'а' => 'a',
+  'б' => 'b',
+  'в' => 'v',
+  'г' => 'g',
+  'д' => 'd',
+  'е' => 'e',
+  'ё' => 'yo',
+  'ж' => 'j',
+  'з' => 'z',
+  'и' => 'i',
+  'й' => 'i',
+  'к' => 'k',
+  'л' => 'l',
+  'м' => 'm',
+  'н' => 'n',
+  'о' => 'o',
+  'п' => 'p',
+  'р' => 'r',
+  'с' => 's',
+  'т' => 't',
+  'у' => 'u',
+  'ф' => 'f',
+  'х' => 'h',
+  'ц' => 'c',
+  'ч' => 'ch',
+  'ш' => 'sh',
+  'щ' => "sh'",
+  'ъ' => '\'',
+  'ы' => 'x',
+  'ь' => 'x',
+  'э' => 'x',
+  'ю' => 'yu',
+  'я' => 'ya'
+];
+//var_dump($translit);
+$enterString = 'Съешь ещё этих французских булок';
+echo "$enterString<br>";
+//$chartest = 'д';
+//echo $translit[$chartest];
+function transLitString($string) {
+  $stringLength = strlen($string);
+//  $charArray = str_split($string);
+  $resultStr = '';
+  $i = 0;
+  while($i < $stringLength) {
+//    echo 'Начало итерации';
+    $char = mb_substr($string, $i, 1);
+//    echo $char;               
+    if ($char == " ") {
+      $resultStr .= "_";
+      $i++;
+    } else {
+//        echo $char;
+      $resultStr .= $translit[$char];
+      $i++;
+    }
+  }
+  echo $resultStr;
+//  var_dump($char);
+};
+
+transLitString($enterString);
+
+//5 задание ----------------------------------
+echo '<br><br>5 задание<br><br>';
+
+$enterString5 = 'File name is correct';
+echo "$enterString5<br>";
+function changeSpaces($string){
+  $stringLength = strlen($string);
+  for ($i=0; $i<$stringLength; $i++) {
+    if ($string[$i] == ' ') {
+      $string[$i] = '_';
+    }
+  }
+  return $string;
+};
+echo changeSpaces($enterString5);
+
+
+
+
+?>
